cfff2660e2cabdcd329ad8cfa1631cdcbd3e7eab
alvin_chien <alvinchien0624@gmail.com>
2018-10-30 21:40:19 -0400

    Updates

---
+++ precache-manifest.6403986cf3d4db330cec81c8b9b4dfc3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3bd857c0333674b1d75",
+    "url": "/my-app/static/js/runtime~main.a3bd857c.js"
+  },
+  {
+    "revision": "d8b358f1b7ee65641fc5",
+    "url": "/my-app/static/js/main.d8b358f1.chunk.js"
+  },
+  {
+    "revision": "7e802ebce41dd84e0e5e",
+    "url": "/my-app/static/js/1.7e802ebc.chunk.js"
+  },
+  {
+    "revision": "d8b358f1b7ee65641fc5",
+    "url": "/my-app/static/css/main.7ce6bd9e.chunk.css"
+  },
+  {
+    "revision": "0d01eee8adb5526a09d705c4c07ba5dc",
+    "url": "/my-app/index.html"
+  }
+];
